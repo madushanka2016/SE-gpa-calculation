05e2d1576685110c154ca3a63a3afa0ba3468095
madushanka2016 <madushankakahawa@gmail.com>
2019-06-25 20:27:15 +0530

    add subject to first Year

--- script/firstYear.js
+++ script/firstYear.js
@@ -177,7 +177,182 @@ function loadFirstYear(){
                         </form>
                     </div>
                 </div>
-                
+                <div class="col">
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 style="padding: 10px 0px 0px 0px">SENG 12213<span style="color: red">C</span> </h6>  
+                                    <p style="padding: 0px 0px 0px 0px">Data Structures And Algorithms</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="subjectMarks" name="marks" onchange="addSubject('SENG 12213',3,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 style="padding: 10px 0px 0px 0px">SENG 12223</h6>
+                                    <p style="padding: 0px 0px 0px 0px">Database Design And Development</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="subjectMarks" name="marks" onchange="addSubject('SENG 12223',3,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 style="padding: 10px 0px 0px 0px">SENG 12233</h6>
+                                    <p style="padding: 0px 0px 0px 0px">Object Oriented Programming</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="subjectMarks" name="marks" onchange="addSubject('SENG 12233',3,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 style="padding: 10px 0px 0px 0px">SENG 12242<span style="color: red">C</span> </h6>  
+                                    <p style="padding: 0px 0px 0px 0px">Management For Software Engineering I</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="subjectMarks" name="marks" onchange="addSubject('SENG 12242',2,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 style="padding: 10px 0px 0px 0px">PMAT 12212</h6>
+                                    <p style="padding: 0px 0px 0px 0px">Discreet Mathematics For Computing II</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="subjectMarks" name="marks" onchange="addSubject('PMAT 12212',2,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 style="padding: 10px 0px 0px 0px">DELT 12312</h6>
+                                    <p style="padding: 0px 0px 0px 0px">Communication Skills For Professionals</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="subjectMarks" name="marks" onchange="addSubject('DELT 12312',2,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
     `
 }
