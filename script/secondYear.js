1120ed7fea9aed226d6d18179daac813f60f0adc
madushanka2016 <madushankakahawa@gmail.com>
2019-06-28 22:22:49 +0530

    add second year js file

---
+++ script/secondYear.js
@@ -0,0 +1,21 @@
+function loadSecondYear(){
+    show.innerHTML = `
+        <div class="row">
+            <div class="col-lg-6 col-md-12">
+                <div>
+                    <h1 style="margin-left: 30%;">First Sem</h1>
+                </div>
+            </div>
+            
+
+
+
+
+            <div class="col-lg-6 col-md-12">
+                <div>
+                    <h1 style="margin-left: 30%;">Second Sem</h1>
+                </div>
+            </div>
+        </div>
+    `
+}
