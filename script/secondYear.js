7edf86e57b1aab7c1e8496c16dfc23e4a0f4dd8c
madushanka2016 <madushankakahawa@gmail.com>
2019-09-16 02:47:44 +0530

    modify

--- script/secondYear.js
+++ script/secondYear.js
@@ -11,8 +11,8 @@ function loadSecondYear(){
                         <form action="">
                             <div class="row">
                                 <div class="col-8">
-                                    <h6 style="padding: 10px 0px 0px 0px">SENG 21213 <span style="color: red"></span> </h6>  
-                                    <p style="padding: 0px 0px 0px 0px">Computer Architecture And Operating Systems</p>
+                                    <h6 class="subjectCode">SENG 21213 <span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Computer Architecture And Operating Systems</p>
                                 </div>
                                 <div class="col-3" style="padding: 15px">
                                     <select class="form-control" id="SENG 21213" name="marks" onchange="addSubject('SENG 21213',3,value)">
@@ -40,8 +40,8 @@ function loadSecondYear(){
                         <form action="">
                             <div class="row">
                                 <div class="col-8">
-                                    <h6 style="padding: 10px 0px 0px 0px">SENG 21222 <span style="color: red"></span> </h6>  
-                                    <p style="padding: 0px 0px 0px 0px">Software Constructions</p>
+                                    <h6 class="subjectCode">SENG 21222 <span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Software Constructions</p>
                                 </div>
                                 <div class="col-3" style="padding: 15px">
                                     <select class="form-control" id="SENG 21222" name="marks" onchange="addSubject('SENG 21222',2,value)">
@@ -69,8 +69,8 @@ function loadSecondYear(){
                         <form action="">
                             <div class="row">
                                 <div class="col-8">
-                                    <h6 style="padding: 10px 0px 0px 0px">SENG 21233 <span style="color: red"></span> </h6>  
-                                    <p style="padding: 0px 0px 0px 0px">Requirement Engineering</p>
+                                    <h6 class="subjectCode">SENG 21233 <span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Requirement Engineering</p>
                                 </div>
                                 <div class="col-3" style="padding: 15px">
                                     <select class="form-control" id="SENG 21233" name="marks" onchange="addSubject('SENG 21233',3,value)">
@@ -98,8 +98,8 @@ function loadSecondYear(){
                         <form action="">
                             <div class="row">
                                 <div class="col-8">
-                                    <h6 style="padding: 10px 0px 0px 0px">SENG 21243 <span style="color: red"></span> </h6>  
-                                    <p style="padding: 0px 0px 0px 0px">Software Modelling</p>
+                                    <h6 class="subjectCode">SENG 21243 <span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Software Modelling</p>
                                 </div>
                                 <div class="col-3" style="padding: 15px">
                                     <select class="form-control" id="SENG 21243" name="marks" onchange="addSubject('SENG 21243',3,value)">
@@ -127,8 +127,8 @@ function loadSecondYear(){
                         <form action="">
                             <div class="row">
                                 <div class="col-8">
-                                    <h6 style="padding: 10px 0px 0px 0px">SENG 21253 <span style="color: red"></span> </h6>  
-                                    <p style="padding: 0px 0px 0px 0px">Web Application Development</p>
+                                    <h6 class="subjectCode">SENG 21253 <span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Web Application Development</p>
                                 </div>
                                 <div class="col-3" style="padding: 15px">
                                     <select class="form-control" id="SENG 21253" name="marks" onchange="addSubject('SENG 21253',3,value)">
@@ -156,8 +156,8 @@ function loadSecondYear(){
                         <form action="">
                             <div class="row">
                                 <div class="col-8">
-                                    <h6 style="padding: 10px 0px 0px 0px">SENG 21263 <span style="color: red">Optionals</span> </h6>  
-                                    <p style="padding: 0px 0px 0px 0px">Interactive Application Development</p>
+                                    <h6 class="subjectCode">SENG 21263 <span style="color: red">Optionals</span> </h6>  
+                                    <p class="subjectName">Interactive Application Development</p>
                                 </div>
                                 <div class="col-3" style="padding: 15px">
                                     <select class="form-control" id="SENG 21263" name="marks" onchange="addSubject('SENG 21263',3,value)">
@@ -185,8 +185,8 @@ function loadSecondYear(){
                         <form action="">
                             <div class="row">
                                 <div class="col-8">
-                                    <h6 style="padding: 10px 0px 0px 0px">SENG 21272 <span style="color: red"></span> </h6>  
-                                    <p style="padding: 0px 0px 0px 0px">Management For Software Engineering II</p>
+                                    <h6 class="subjectCode">SENG 21272 <span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Management For Software Engineering II</p>
                                 </div>
                                 <div class="col-3" style="padding: 15px">
                                     <select class="form-control" id="SENG 21272" name="marks" onchange="addSubject('SENG 21272',2,value)">
@@ -214,8 +214,8 @@ function loadSecondYear(){
                         <form action="">
                             <div class="row">
                                 <div class="col-8">
-                                    <h6 style="padding: 10px 0px 0px 0px">SENG 24213 <span style="color: red"></span> </h6>  
-                                    <p style="padding: 0px 0px 0px 0px">Computer Netwokrs</p>
+                                    <h6 class="subjectCode">SENG 24213 <span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Computer Netwokrs</p>
                                 </div>
                                 <div class="col-3" style="padding: 15px">
                                     <select class="form-control" id="SENG 24213" name="marks" onchange="addSubject('SENG 24213',3,value)">
@@ -245,7 +245,153 @@ function loadSecondYear(){
                     <div>
                         <h1 style="margin-left: 30%;">Second Sem</h1>
                     </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 class="subjectCode">SENG 22212 <span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Software Architecture and Design</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="SENG 22212" name="marks" onchange="addSubject('SENG 22212',2,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 class="subjectCode">SENG 22223 <span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Human Computer Interaction</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="SENG 22223" name="marks" onchange="addSubject('SENG 22223',3,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 class="subjectCode">SENG 22233 <span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Software Verification and Validation</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="SENG 22233" name="marks" onchange="addSubject('SENG 22233',3,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 class="subjectCode">SENG 22243 <span style="color: red">Optionals</span> </h6>  
+                                    <p class="subjectName">Embedded Systems Development</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="SENG 22243" name="marks" onchange="addSubject('SENG 22243',3,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 class="subjectCode">PMAT 22213 <span style="color: red">Optionals</span> </h6>  
+                                    <p class="subjectName">Mathematical Methods</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="PMAT 22213" name="marks" onchange="addSubject('PMAT 22213',3,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
     `
+    load(2);
 }
