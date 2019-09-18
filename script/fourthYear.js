9e57311b05b08417005df2b867dd4773d6be8ff6
madushanka2016 <madushankakahawa@gmail.com>
2019-09-18 23:09:26 +0530

    add sencond subject

--- script/fourthYear.js
+++ script/fourthYear.js
@@ -393,11 +393,11 @@ function loadFourthYear(){
                         <form action="">
                             <div class="row">
                                 <div class="col-8">
-                                    <h6 class="subjectCode">SENG 12213<span style="color: red"></span> </h6>  
-                                    <p class="subjectName">Data Structures And Algorithms</p>
+                                    <h6 class="subjectCode">SENG 42212<span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Software Safety and Reliability</p>
                                 </div>
                                 <div class="col-3" style="padding: 15px">
-                                    <select class="form-control" id="SENG 12213" name="marks" onchange="addSubject('SENG 12213',3,value)">
+                                    <select class="form-control" id="SENG 42212" name="marks" onchange="addSubject('SENG 42212',2,value)">
                                         <option value="none">--</option>
                                         <option value=4.0>A+</option>
                                         <option value=4.0>A</option>
@@ -422,11 +422,11 @@ function loadFourthYear(){
                         <form action="">
                             <div class="row">
                                 <div class="col-8">
-                                    <h6 class="subjectCode">SENG 12223</h6>
-                                    <p class="subjectName">Database Design And Development</p>
+                                    <h6 class="subjectCode">SENG 43216</h6>
+                                    <p class="subjectName">Software Engineering Research Project</p>
                                 </div>
                                 <div class="col-3" style="padding: 15px">
-                                    <select class="form-control" id="SENG 12223" name="marks" onchange="addSubject('SENG 12223',3,value)">
+                                    <select class="form-control" id="SENG 43216" name="marks" onchange="addSubject('SENG 43216',6,value)">
                                         <option value="none">--</option>
                                         <option value=4.0>A+</option>
                                         <option value=4.0>A</option>
@@ -451,11 +451,11 @@ function loadFourthYear(){
                         <form action="">
                             <div class="row">
                                 <div class="col-8">
-                                    <h6 class="subjectCode">SENG 12233</h6>
-                                    <p class="subjectName">Object Oriented Programming</p>
+                                    <h6 class="subjectCode">SENG 42222</h6>
+                                    <p class="subjectName">Usability Engineering</p>
                                 </div>
                                 <div class="col-3" style="padding: 15px">
-                                    <select class="form-control" id="SENG 12233" name="marks" onchange="addSubject('SENG 12233',3,value)">
+                                    <select class="form-control" id="SENG 42222" name="marks" onchange="addSubject('SENG 42222',2,value)">
                                         <option value="none">--</option>
                                         <option value=4.0>A+</option>
                                         <option value=4.0>A</option>
@@ -480,11 +480,11 @@ function loadFourthYear(){
                         <form action="">
                             <div class="row">
                                 <div class="col-8">
-                                    <h6 class="subjectCode">SENG 12242<span style="color: red"></span> </h6>  
-                                    <p class="subjectName">Management For Software Engineering I</p>
+                                    <h6 class="subjectCode">SENG 42232<span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Software Management</p>
                                 </div>
                                 <div class="col-3" style="padding: 15px">
-                                    <select class="form-control" id="SENG 12242" name="marks" onchange="addSubject('SENG 12242',2,value)">
+                                    <select class="form-control" id="SENG 42232" name="marks" onchange="addSubject('SENG 42232',2,value)">
                                         <option value="none">--</option>
                                         <option value=4.0>A+</option>
                                         <option value=4.0>A</option>
@@ -509,11 +509,11 @@ function loadFourthYear(){
                         <form action="">
                             <div class="row">
                                 <div class="col-8">
-                                    <h6 class="subjectCode">PMAT 12212</h6>
-                                    <p class="subjectName">Discreet Mathematics For Computing II</p>
+                                    <h6 class="subjectCode">SENG 42242</h6>
+                                    <p class="subjectName">Machine Learning</p>
                                 </div>
                                 <div class="col-3" style="padding: 15px">
-                                    <select class="form-control" id="PMAT 12212" name="marks" onchange="addSubject('PMAT 12212',2,value)">
+                                    <select class="form-control" id="SENG 42242" name="marks" onchange="addSubject('SENG 42242',2,value)">
                                         <option value="none">--</option>
                                         <option value=4.0>A+</option>
                                         <option value=4.0>A</option>
@@ -538,11 +538,185 @@ function loadFourthYear(){
                         <form action="">
                             <div class="row">
                                 <div class="col-8">
-                                    <h6 class="subjectCode">DELT 12312</h6>
-                                    <p class="subjectName">Communication Skills For Professionals</p>
+                                    <h6 class="subjectCode">SENG 42252</h6>
+                                    <p class="subjectName">Computer Graphics</p>
                                 </div>
                                 <div class="col-3" style="padding: 15px">
-                                    <select class="form-control" id="DELT 12312" name="marks" onchange="addSubject('DELT 12312',2,value)">
+                                    <select class="form-control" id="SENG 42252" name="marks" onchange="addSubject('SENG 42252',2,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 class="subjectCode">SENG 42273<span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Semantic Web and Ontological Engineering</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="SENG 42273" name="marks" onchange="addSubject('SENG 42273',3,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 class="subjectCode">SENG 42283<span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Mobile Networks</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="SENG 42283" name="marks" onchange="addSubject('SENG 42283',3,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 class="subjectCode">SENG 42293<span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Big Data Analytics</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="SENG 42293" name="marks" onchange="addSubject('SENG 42293',3,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 class="subjectCode">SENG 42303<span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Medical Imaging and Biomedial Signal Processing</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="SENG 42303" name="marks" onchange="addSubject('SENG 42303',3,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 class="subjectCode">SENG 42313<span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Advanced Topics in Game Design and Animation</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="SENG 42313" name="marks" onchange="addSubject('SENG 42313',3,value)">
+                                        <option value="none">--</option>
+                                        <option value=4.0>A+</option>
+                                        <option value=4.0>A</option>
+                                        <option value=3.7>A-</option>
+                                        <option value=3.3>B+</option>
+                                        <option value=3.0>B</option>
+                                        <option value=2.7>B-</option>
+                                        <option value=2.3>C+</option>
+                                        <option value=2.0>C</option>
+                                        <option value=1.7>C-</option>
+                                        <option value=1.3>D+</option>
+                                        <option value=1.0>D</option>
+                                        <option value=0.7>D-</option>
+                                    </select>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="border border-success" style="padding: 0px 20px 0px 20px;
+                        margin: 10px 20px 10px 20px;
+                        border-radius: 25px">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-8">
+                                    <h6 class="subjectCode">SENG 42323<span style="color: red"></span> </h6>  
+                                    <p class="subjectName">Business Process Engineering</p>
+                                </div>
+                                <div class="col-3" style="padding: 15px">
+                                    <select class="form-control" id="SENG 42323" name="marks" onchange="addSubject('SENG 42323',3,value)">
                                         <option value="none">--</option>
                                         <option value=4.0>A+</option>
                                         <option value=4.0>A</option>
